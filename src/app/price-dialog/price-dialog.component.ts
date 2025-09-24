f020cb1e53b72ada308efce951dd8dec1f0943a6
hannakpa <petrakovahanna@gmail.com>
2025-09-24 11:27:41 +0200

    cypress_&_newIcon_&_materialDependency

---
+++ src/app/price-dialog/price-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './price-dialog.component.html',
+  styleUrl: './price-dialog.component.css'
+})
+export class PriceDialogComponent {
+
+}
